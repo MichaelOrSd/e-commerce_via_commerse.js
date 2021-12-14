664f59ebad93ffd20cae4c5b815bc7d06e6de3ef
Michael O'Reilly <86013664+MichaelOrSd@users.noreply.github.com>
2021-12-14 12:32:40 -0330

    NL Pawn Shop

---
+++ src/components/Products/Product/styles.js
@@ -0,0 +1,22 @@
+// css styles go here
+
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  root: {
+    maxWidth: "96%",
+    maxHeight: "96%",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
